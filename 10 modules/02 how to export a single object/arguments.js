b9df6ac3de6f59a229a03e6e5acd831747606566
mholzmann <m.holzmann@htl-leonding.ac.at>
2021-01-25 19:08:35 +0100

    edit demo

--- 10 modules/02 how to export a single object/arguments.js
+++ 10 modules/02 how to export a single object/arguments.js
@@ -3,21 +3,21 @@ const typedArgs = args.map(convertDataType);
 
 /**
  * Tries to convert the given string into a boolean or a number.
- * @param {string} text 
+ * @param {string} string 
  */
-function convertDataType(text) {
+function convertDataType(string) {
     // return a boolean
-    const lowerCase = text.toLowerCase();
+    const lowerCase = string.toLowerCase();
     if (lowerCase === 'true' || lowerCase === 'false') {
         return lowerCase === 'true';
     }
     // return a number
-    const number = Number(text);
+    const number = Number(string);
     if (!Number.isNaN(number)) {
         return number;
     }
     // return a string
-    return text;
+    return string;
 }
 
 // a single object or function can be exported by assigning it to the module.exports property
