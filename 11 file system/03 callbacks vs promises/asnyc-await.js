0bc8f9b110a72de306f7f97d62ee082cde2001a2
mholzmann <m.holzmann@htl-leonding.ac.at>
2021-02-17 07:58:01 +0100

    add promises und json

---
+++ 11 file system/03 callbacks vs promises/asnyc-await.js
@@ -0,0 +1,25 @@
+// With promises you can avoid callback hell (see callback.js).
+// The keywords 'async' and 'await' make the usage of promises more comfortable.
+
+const fsPromises = require('fs').promises;
+
+async function mergeFiles() {
+    try {
+        console.log('Reading part1.txt ...');
+        let text = await fsPromises.readFile('./part1.txt', 'utf8');
+        console.log('Read file successfully.');
+        
+        console.log('Reading part2.txt ...');
+        text += await fsPromises.readFile('./part2.txt', 'utf8');
+        console.log('Read file successfully.');        
+
+        console.log('Writing file ...');
+        await fsPromises.writeFile('./merged-with-promises.txt', text);
+        console.log('Merged files successfully.');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+mergeFiles();
+console.log('Could do something else meanwhile ...');
---
+++ 11 file system/03 callbacks vs promises/callbacks.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+fs.readFile('./part1.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        let text = data;
+        fs.readFile('./part2.txt', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err.message);
+            } else {
+                text += data;
+
+                fs.writeFile('./merged-with-callbacks.txt', text, (err) => {
+                    if (err) {
+                        console.error(err.message);
+                    } else {
+                        console.log('Merged files successfully.');
+                    }
+                });        
+            }
+        });
+    }
+});
+
+// Those nested callbacks are called "callback hell" ...
