6e127668bb88a32b8f54860d679737fc27663232
mholzmann <ubuntu@TABLET-MOO5AB3M.localdomain>
2021-04-07 07:45:57 +0200

    replace getElementById with querySelector

--- 12 fetch api/02 how to handle http errors/pokeapi.js
+++ 12 fetch api/02 how to handle http errors/pokeapi.js
@@ -10,21 +10,22 @@ document.querySelector('#catch-button').addEventListener('click', loadPokemon);
 */
 async function loadPokemon() {
     document.querySelector('#result').innerHTML = ""
-    const name = document.getElementById('pokemon').value;
+    const name = document.querySelector('#pokemon').value;
 
     try {
         // Trying to fetch a pokemon which might not exist
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}/`);
 
         if (response.ok) {
+            // Executed if the response was successfull (HTTP status code in the range 200-299)
             const pokemon = await response.json();
             document.getElementById('result').innerHTML = `You catched a ${pokemon.name} with a height of ${pokemon.height} decimetres.`;
         } else {
-            // Executed e.g. if server returns HTTP Code 404 (NOT FOUND)
+            // Executed if the response fails e.g. if server returns HTTP status code 404 (NOT FOUND)
             alert(`Error: Could not fetch Pokemon "${name}"`);
         }
     } catch (error) {
-        // executed e.g. if there is no network connection
+        // Executed if the server doesn't response e.g. if there is no network connection
         alert(error);
     }
 }
