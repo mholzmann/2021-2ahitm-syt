264feca3688feec2a340a45698eb6583d2b4d6ae
mholzmann <ubuntu@TABLET-MOO5AB3M.localdomain>
2021-04-21 10:45:57 +0200

    add first express server demo

---
+++ 13 express/01 how to create a webserver/app.js
@@ -0,0 +1,30 @@
+// import modules
+const express = require('express'); 
+const path = require('path');
+
+// create express application
+const app = express();  
+
+// specify http server port
+const port = 3000;
+
+// respond with "hello world" to a HTTP GET request at route '/'
+app.get('/', (req, res) => {
+    const file = path.join(__dirname, './public/index.html');
+    res.sendFile(file);
+});    
+
+// respond with "hello john" to a HTTP GET request at route '/john'
+app.get('/john', (req, res) => {
+    res.status(200).send('hello john');
+});        
+
+// respond with "hello <name>" to a HTTP GET request at route '/<name>' by using a route parameter for capturing the value from the URL
+app.get('/:name', (req, res) => {
+    res.send(`hi ${req.params.name}`);
+});
+
+// start http server
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
